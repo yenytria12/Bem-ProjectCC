3f39fc88dd16e21e810f9d5afa6cbc295cf39db5
yenytrias2 <yenytrias2@gmail.com>
2025-12-22 00:25:26 +0700

    Add Azure nginx config

--- startup.sh
+++ startup.sh
@@ -1,27 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-echo ">>> MEMULAI STARTUP SCRIPT..."
+# Copy nginx config
+cp /home/site/wwwroot/default /etc/nginx/sites-available/default
 
-# 1. Pastikan folder public ada
-if [ -d "/home/site/wwwroot/public" ]; then
-    echo ">>> Folder public ditemukan."
-else
-    echo ">>> WARNING: Folder public TIDAK ditemukan!"
-fi
+# Run Laravel optimizations
+cd /home/site/wwwroot
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
-# 2. Benerin Nginx Root & Routing
-echo ">>> Memperbaiki konfigurasi Nginx..."
-cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/public;|g' /etc/nginx/sites-available/default
-sed -i 's|index  index.php index.html index.htm hostingstart.html;|index  index.php index.html index.htm hostingstart.html; try_files $uri $uri/ /index.php?$query_string;|g' /etc/nginx/sites-available/default
-
-# 3. Reload Nginx
-echo ">>> Reloading Nginx..."
+# Restart nginx
 service nginx reload
-
-# 4. Jalanin Migrasi Otomatis (Opsional tapi berguna)
-# cd /home/site/wwwroot && php artisan migrate --force
-
-# 5. Start PHP
-echo ">>> Starting PHP-FPM..."
-php-fpm

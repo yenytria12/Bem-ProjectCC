52a8c205dacb08a218f98460403590803666ff65
yenytria12 <yenytrias2@gmail.com>
2026-01-04 09:34:25 +0700

    fix: add MYSQL_ATTR_SSL_VERIFY_SERVER_CERT=false for Azure MySQL SSL

--- config/database.php
+++ config/database.php
@@ -57,11 +57,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? (env('MYSQL_ATTR_SSL_CA') ? [
+            'options' => extension_loaded('pdo_mysql') && env('MYSQL_ATTR_SSL_CA') ? [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ] : [
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
